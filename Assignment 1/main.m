4a67ea76f65724d8c8531226094d56730eba0bbc
sai1999gaurav <46236635+sai1999gaurav@users.noreply.github.com>
2020-01-28 19:52:56 +0530

    modified--still not working

--- Assignment 1/main.m
+++ Assignment 1/main.m
@@ -1,27 +1,27 @@
 
 %% %Problem 1 (b)
-A=input('Input Matrix :\n');
-[n,m] = size(A);
+V_thresh = 20e-3;
+El = -70e-3;
+I=input('Input Current Matrix :\n');
 T = input('total time of consideration : \n');
-V_t = runge_kutta(A, n, m, T/m);
+V_t = runge_kutta(I, V_thresh, El, T/size(I,2));
 disp(V_t);
 %% Problem 1(c)
+V_thresh = 20e-3;
+El = -70e-3;
 alpha = 0.1;
 T = 500; % in ms
 del_t = 0.1;
 I_c = 2.7e-9; % calculated in part 1 (a)
-m = T/del_t; % 500/0.1;
-n = 4;
+m = (T/del_t); % 500/0.1 + 1;
+n = 10;
 I = ones(n,1);
 for i = 1:n
-    I(i,1) = (1 + 2*i*alpha)*I_c;
+    I(i,1) = (1 + i*alpha)*I_c;
 end
-A = zeros(n, m+1);
-A(:,(2:m+1)) = I.*ones(1,m);
-V_t = runge_kutta(A, n, m, del_t);
+A = I.*ones(1,m);
+V_t = runge_kutta(A, V_thresh, El, del_t);
 t = 0:0.1:500;
- for i = 1:n
- plot(t, V_t(i,:), 'Linewidth', 2);
- hold on;
- end
+%disp(size(V_t));
+plot(V_t(2,:), 'Linewidth', 2);
  
